dcaa55598bf133b62fbf4dc63283951174afff1f
Victoria Do <victoriado@outlook.com>
2015-01-24 22:00:06 -0800

    Adding placeholder image and some animations

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,8 +6,28 @@ var uisControllers = angular.module('uisControllers', []);
 
 uisControllers.controller('HomePageCtrl', ['$scope', 
   function($scope, HomePage) {
-    
-  }]);
+    $scope.minOpacity = 0.4;
+    $scope.targetDelta = 0.6; // minOpacity + targetDelta = max opacity
+    $scope.onScroll = function(element, opacity) {
+      element.css({
+        'background':  
+          'radial-gradient(500px at center, rgba(0, 111, 175, ' + ((opacity-0.4)/2 + 0.4) + '), rgba(21, 28, 48, ' + opacity + ')), url("../../assets/img/attention.jpg") 50% 20%',
+        'background-size': 'cover'
+      });
+    }
+  }])
+  .directive("uisOpacity", function ($window) {
+      return function(scope, element, attrs) {
+          var activeDistance = 400;     
+          angular.element($window).bind("scroll", function() {
+              var delta = Math.max(0, this.pageYOffset - element[0].scrollTop);
+              var opacity = Math.min(1, scope.minOpacity + (delta * scope.targetDelta)/500);
+              scope.onScroll(element, opacity);
+        
+              scope.$apply(); 
+          });        
+      };
+  });
 
 uisControllers.controller('RecruitingPageCtrl', ['$scope', 
   function($scope, RecruitingPage) {
