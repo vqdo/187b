3ebb09f4e3bf399ebd20669e7aeed8d6bb75f58e
Victoria Do <victoriado@outlook.com>
2015-02-12 10:27:34 -0800

    Configuring mail to work with godaddy

--- app/php/mail.php
+++ app/php/mail.php
@@ -1,34 +1,13 @@
 <?php 
-
-    try {
-        // Pear Mail Library
-        require_once "Mail.php";
-        
         $postdata = file_get_contents("php://input");
         $request = json_decode($postdata);
     
         $from = $request->name;
-        $to = 'victoriado@outlook.com';
-        $subject = 'UIS Contact Form: ' . $request->name;
-        $body = "From: $request->name \n Email: $request->email \n $request->message";
-        
-        $headers = array(
-            'From' => $from,
-            'To' => $to,
-            'Subject' => $subject
-        );
-        
-        $smtp = Mail::factory('smtp');
-        
-        $mail = $smtp->send($to, $headers, $body);
-        
-        if (PEAR::isError($mail)) {
-            echo('Bad: ' . $mail->getMessage() . '');
-        } else {
-            echo('<p>Message successfully sent!</p>');
-        }            
-    } catch(Exception $e) {
-        echo "PEAR Mail unavailable. Message not sent.";
-    }
+        $to = 'uis.ucsd@gmail.com';
+        $subject = 'Message from : ' . $request->name;
+        $body = "Sent via uisucsd.com. Do not reply to this email.\n\nFrom: $request->name \n Email: $request->email \n\n $request->message";
 
+        $success = mail($to, $subject, $body);
+        echo $success;
+    
 ?>
